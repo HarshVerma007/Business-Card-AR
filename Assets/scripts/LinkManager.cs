589baf354aaaf7afa8f11b6ee6fd6b3a5cedc169
HarshVerma007 <harshverma0502@gmail.com>
2023-07-09 11:20:55 +0530

    files added

---
+++ Assets/scripts/LinkManager.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LinkManager : MonoBehaviour
+{
+     public void openLinkedin()
+     {
+        Application.OpenURL("https://www.linkedin.com/in/harsh-verma-b41a8b1bb/");
+     }
+
+     public void openinstagram()
+     {
+        Application.OpenURL("https://www.instagram.com/ig._.harshop/");
+     }
+
+     public void openGithub()
+     {
+        Application.OpenURL("https://github.com/HarshVerma007");
+     }
+
+     public void openTwitter()
+     {
+        Application.OpenURL("https://twitter.com/harshverma0502");
+     }
+
+     public void openYoutube()
+     {
+        Application.OpenURL("https://www.youtube.com/channel/UCQ4ZYn6PbMyaSjBF43YQ4UA");
+     }
+}
